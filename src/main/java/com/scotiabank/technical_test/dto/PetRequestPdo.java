6a2cf94b4a09077b4a6d55bd36961f2988e0c0bf
Antonio Zarate Rosas <azr230502@gmail.com>
2025-02-24 14:38:54 -0600

    Validations in PetRequestPdo with custom error messages

--- src/main/java/com/scotiabank/technical_test/dto/PetRequestPdo.java
+++ src/main/java/com/scotiabank/technical_test/dto/PetRequestPdo.java
@@ -1,10 +1,19 @@
 package com.scotiabank.technical_test.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class PetRequestPdo {
+    @Min(value = 1, message = "El id de la mascota no puede ser menor a 1")
+    @NotNull(message = "El id de la mascota no puede ser nulo")
     private Integer id;
+
+    @NotBlank(message = "El campo 'status' no puede ir en blanco ni ser nulo")
     private String status;
+
+    @NotBlank(message = "El campo de 'name' no puede ir en blanco ni ser nulo")
     private String name;
 }
